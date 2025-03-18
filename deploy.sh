7ce56cd706376a26fb6760e57b76c70db093eafd
gedeononka <lovegedeonisidk@groupeisi.com>
2025-03-18 20:39:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,14 @@
 
 # Variables
 CONTAINER_NAME="labisi2025"
-IMAGE_NAME="dockergedeon4/labisi2025:latest"  # Corrigé : ajout du préfixe username DockerHub correctement
+IMAGE_NAME="gedeon4/labisi2025:latest"
 
-# Arrêt et suppression du conteneur existant
-sudo docker stop "$CONTAINER_NAME" 2>/dev/null || true
-sudo docker rm "$CONTAINER_NAME" 2>/dev/null || true
+# Arrêt du conteneur existant
+sudo docker stop $CONTAINER_NAME || true
+sudo docker rm $CONTAINER_NAME || true
 
 # Suppression de l'image existante
-sudo docker rmi "$IMAGE_NAME" 2>/dev/null || true
+sudo docker rmi $IMAGE_NAME || true
 
-# Téléchargement de l'image et exécution du conteneur
-sudo docker pull "$IMAGE_NAME"
-sudo docker run -d -p 80:80 --name "$CONTAINER_NAME" "$IMAGE_NAME"
+# Exécution du conteneur
+sudo docker run -d -p 80:80 --name $CONTAINER_NAME $IMAGE_NAME
